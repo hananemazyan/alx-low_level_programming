80765be30550b85a6e035b60c559a097cc56068e
Hanane mazyan <hananemzn16@gmail.com>
2023-04-17 17:51:58 -0700

    Mandatory

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,8 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x)<(0)? -(x):(x))
-/* ? the ternary operator to check 
-   * if x is less than 0
-   */
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+/* ? the ternary operator to check if x is less than 0 */
 #endif
