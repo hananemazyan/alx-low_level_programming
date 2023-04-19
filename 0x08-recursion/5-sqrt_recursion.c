9787a38f1676225bcaa199109eff084a799f2ae9
Hanane mazyan <hananemzn16@gmail.com>
2023-04-18 19:23:39 -0700

    Mandatory

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,7 +4,8 @@
   * _sqrt_helper - recurse to find the natural square root ofa number
   * @n: number to calculate the square root
   * @i: The possible value of square root
-  * Return: -1 if n does not have a natural square root, otherwise , the natural square root of n
+  * Return: -1 if n does not have a natural square root
+  * otherwise , the natural square root of n
   */
 int _sqrt_helper(int i, int n)
 {
@@ -17,14 +18,14 @@ int _sqrt_helper(int i, int n)
 
 
 /**
- *_sqrt_recursion -  returns the natural square root of a number
- * @n: number 
+ *_sqrt_recursion - returns the natural square root of a number
+ * @n: number
  * Return: naturel square of a number.
  */
 
 int _sqrt_recursion(int n)
 {
-	if(n < 0)
+	if (n < 0)
 		return (-1);
-	return (_sqrt_helper(0,n));
+	return (_sqrt_helper(0, n));
 }
